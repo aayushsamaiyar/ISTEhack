f1866f7d35758c799bb88a4088a96320fb3c57c2
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-04-10 01:45:00 +0530

    Add files via upload

---
+++ client/src/components/HomeComp/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+	return(
+		<>
+			<div className="head">
+				<div className="symbol">
+				</div>
+				<div className="title">
+				</div>
+				<div className="line">
+				</div>
+				<div className="are">
+				</div>
+				<div className="sign">
+				</div>
+				<div className="quote">
+				</div>
+				<div className="quote2">
+				</div>
+				<input type="text" placeholder="SEARCH FOR DOCTOR"/>
+				<div className="spec">
+				</div>
+				<div className="line3">
+				</div>
+				<div className="frame3">
+				</div>
+				<div className="frame4">
+				</div>
+				<div className="frame5">
+				</div>
+				<div className="frame6">
+				</div>
+				<div className="frame8">
+				</div>
+				<div className="frame9">
+				</div>
+				<div className="frame10">
+				</div>
+				<div className="frame11">
+				</div>
+				<div className="line4">
+				</div>
+			</div>
+		</>
+		);
+}
+
+export default Home;
