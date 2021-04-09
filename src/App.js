fa3946c85d79025b4c0cab7239105b3649425ef0
Shreyash Vardhan <shreyashfeb@gmail.com>
2021-04-09 18:45:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Midhome from './components/MiddleHomeComp/Midhome';
 
+import Navbar from './components/NavbarComp/Navbar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar/>
+        <Midhome/>
     </div>
   );
 }
---
+++ src/components/MiddleHomeComp/Midhome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function  Midhome() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Midhome
