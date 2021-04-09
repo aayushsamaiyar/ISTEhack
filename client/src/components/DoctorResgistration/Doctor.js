59c5de77c5d65ae7cc5eaaab49174f7d12f938fa
Dhruvil Dave <67459224+Dhruvil-Dave@users.noreply.github.com>
2021-04-10 01:45:55 +0530

    Add files via upload

---
+++ client/src/components/DoctorResgistration/Doctor.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Doctor.css';
+
+const Doctor = () =>{
+	return(
+
+		<>
+			<div className="head">
+				<div className="registration">
+				Doctors Registration
+				</div>
+				<div className="linetop">
+				</div>
+				<div className="lineno13">
+				</div>
+				<div className="firstName">
+				</div>
+				<div className="lastName">
+				</div>
+				<div className="userName">
+				</div>
+				<div className="password">
+				</div>
+				<div className="speciality">
+				</div>
+				<div className="address">
+				</div>
+				<div className="clinicName">
+				</div>
+				<div className="aboutyou">
+				</div>
+				<div className="education">
+				</div>
+				<div className="opentime">
+				</div>
+				<div className="closetime">
+				</div>
+			</div>
+		</>
+
+		);
+}
+
+export default Doctor;
