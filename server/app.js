9539f6770568ea4ac5fd68c068330ae720ea30cd
aayushsamaiyar <68498029+aayushsamaiyar@users.noreply.github.com>
2021-04-10 20:44:53 +0530

    file upload added

--- server/app.js
+++ server/app.js
@@ -2,25 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const cors = require('cors');
+
 const app = express()
- 
+
 const auth = require('./routes/auth');
+const fileRoutes = require('./routes/file-upload-routes');
 const passport = require('passport');
 const passportConfig = require('./routes/passport');
-// const Doctor = require('./models/Doctor');
-// const Patient = require('./models/patient');
+const Doctor = require('./models/Doctor');
+const Patient = require('./models/Patients');
 const isDoctor = require('./isDoctor');
 const isPatient = require('./isPatient');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://AayushSamaiyar:${process.env.PASSWORD}@cluster0.awh5s.mongodb.net/tes',{
+app.use(cookieParser());
+mongoose.connect('mongodb+srv://AayushSamaiyar:${process.env.PASSWORD}@cluster0.awh5s.mongodb.net/test',{
     useNewUrlParser:true, 
-    useUnifiedTopology:true}, ()  => {
+    useUnifiedTopology:true, 
+    useCreateIndex: true,
+    useFindAndModify: true}, ()  => {
         console.log('Connected to database');
 })
 
 app.use('/user',auth);
-// app.use('/Doctors',passport.authenticate('jwt',{session:false}), isDoctor,Doctor);
-// app.use('/patient',passport.authenticate('jwt',{session:false}),isPatient,Patient);
+app.use('/Doctors',passport.authenticate('jwt',{session:false}), isDoctor,Doctor);
+app.use('/patient',passport.authenticate('jwt',{session:false}),isPatient,Patient);
 
 app.get('/', (req,res) => {
     res.status(200).json({message : 'Welcome'})
@@ -33,6 +41,10 @@ app.get('/login',(req,res)=>{
 app.get('/register',(req,res)=>{
     res.status(200).json({message:'Welcome to register page'});
 })
+
+app.use(bodyParser.json())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api', fileRoutes.routes);
 
 const port = process.env.PORT || 5000
 
--- server/models/Patients.js
+++ server/models/Patients.js
@@ -1,4 +1,4 @@
-const mongoose = require('express');
+const mongoose = require('mongoose');
 
 const PatientSchema = new mongoose.Schema({
     username:{
